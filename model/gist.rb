6f0109eb4f1cec03e75d97e98dc529c399077138
Phil Wittig <pdwittig@gmail.com>
2014-03-09 19:41:12 -0700

    forgot to add reader for filename

--- model/gist.rb
+++ model/gist.rb
@@ -2,7 +2,7 @@
 
 
 class Gist
-  attr_reader :file, :url, :id, :num_of_comments, :most_recent_comment_date
+  attr_reader :filename, :url, :id, :num_of_comments, :most_recent_comment_date
   
   def initialize( filename, url, id, num_of_comments, most_recent_comment_date )  
     @filename                    = filename
