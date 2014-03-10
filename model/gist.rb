7a309720a9f8bb1d161b1e96e4818b959af01371
Phil Wittig <pdwittig@gmail.com>
2014-03-09 19:39:23 -0700

    added short_filename method

--- model/gist.rb
+++ model/gist.rb
@@ -4,12 +4,15 @@
 class Gist
   attr_reader :file, :url, :id, :num_of_comments, :most_recent_comment_date
   
-  def initialize( file, url, id, num_of_comments, most_recent_comment_date )  
-    @file                     = file
-    @url                      = url
-    @id                       = id
-    @num_of_comments          = num_of_comments
-    @most_recent_comment_date = most_recent_comment_date
+  def initialize( filename, url, id, num_of_comments, most_recent_comment_date )  
+    @filename                    = filename
+    @url                         = url
+    @id                          = id
+    @num_of_comments             = num_of_comments
+    @most_recent_comment_date    = most_recent_comment_date
   end
   
+  def short_filename
+  	return filename.slice(0...30) << '...'
+  end
 end
